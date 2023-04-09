9affb1fc374eb4bd5f0a355af54042eee20cba8a
Khadem <a.khadem@digikala.com>
2023-04-09 16:38:50 +0330

    refactor(hello_test): Add assertCorrectMessage helper function to make code more readable

--- hello_test.go
+++ hello_test.go
@@ -6,27 +6,30 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		name := "alireza"
 		exceptedOutput := englishHelloPrefix + name
-
 		output := Hello(name)
-		if output != exceptedOutput {
-			t.Errorf(
-				"excepted %q got %q",
-				exceptedOutput,
-				output,
-			)
-		}
+
+		assertCorrectMessage(t, output, exceptedOutput)
 	})
 	t.Run("saying 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		emptyString := ""
 		exceptedOutput := englishHelloPrefix + "World"
-
 		output := Hello(emptyString)
-		if output != exceptedOutput {
-			t.Errorf(
-				"excepted %q got %q",
-				exceptedOutput,
-				output,
-			)
-		}
+
+		assertCorrectMessage(t, output, exceptedOutput)
 	})
+}
+
+func assertCorrectMessage(
+	t testing.TB,
+	output string,
+	exceptedOutput string,
+) {
+	t.Helper()
+	if output != exceptedOutput {
+		t.Errorf(
+			"excepted %q got %q",
+			exceptedOutput,
+			output,
+		)
+	}
 }
