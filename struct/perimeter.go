26e8cd99bfb97dde2d14a775f4f411cb67b9cb85
Khadem <a.khadem@digikala.com>
2023-04-09 23:59:28 +0330

    refactor: Define Circle struct. Change Area and Perimeter function to methods that can be called over both of rectangle and circle instance

--- struct/perimeter.go
+++ struct/perimeter.go
@@ -1,14 +1,24 @@
 package struct_
 
+import "math"
+
 type Rectangle struct {
 	width  float64
 	height float64
 }
 
-func Perimeter(rectangle Rectangle) (perimeter float64) {
+type Circle struct {
+	radius int
+}
+
+func (rectangle Rectangle) Perimeter() (perimeter float64) {
 	return (rectangle.width + rectangle.height) * 2
 }
 
-func Area(rectangle Rectangle) (area float64) {
+func (rectangle Rectangle) Area() (area float64) {
 	return rectangle.width * rectangle.height
+}
+
+func (circle Circle) Area() (area float64) {
+	return math.Pi * math.Pow(float64(circle.radius), 2)
 }
--- struct/perimeter_test.go
+++ struct/perimeter_test.go
@@ -9,7 +9,7 @@ func TestPerimeter(t *testing.T) {
 	}
 	exceptedOutput := 40.0
 
-	output := Perimeter(rectangle)
+	output := rectangle.Perimeter()
 	if output != exceptedOutput {
 		t.Errorf(
 			"got %.2f want %.2f",
@@ -20,18 +20,32 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{
-		width:  6.0,
-		height: 12.0,
-	}
-	exceptedOutput := 72.0
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{
+			width:  6.0,
+			height: 12.0,
+		}
+		exceptedOutput := 72.0
 
-	output := Area(rectangle)
-	if output != exceptedOutput {
-		t.Errorf(
-			"got %.2f want %.2f",
-			output,
-			exceptedOutput,
-		)
-	}
+		output := rectangle.Area()
+		if output != exceptedOutput {
+			t.Errorf(
+				"got %.2f want %.2f",
+				output,
+				exceptedOutput,
+			)
+		}
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{
+			radius: 10,
+		}
+		exceptedOutput := 314.1592653589793
+		output := circle.Area()
+
+		if output != exceptedOutput {
+			t.Errorf("got %g want %g", output, exceptedOutput)
+		}
+	})
 }
